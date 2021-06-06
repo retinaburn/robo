ef76bef70a45589510fe36d4dbcd46cbce3361ea
Craig Moynes <craig.moynes@gmail.com>
2021-06-06 01:18:05 -0400

    Add arm control with sticks
    Added dedup events so we dont go crazy sending events

--- robodriver.py
+++ robodriver.py
@@ -26,7 +26,8 @@ MENU_BUTTON = 7
 B_BUTTON = 1
 A_BUTTON = 0
 DPAD = JOYHATMOTION
-
+LEFTSTICK_UPDOWN = 1
+RIGHTSTICK_UPDOWN = 4
 
 # Input Loop Definitions
 IS_RUNNING = True
@@ -40,19 +41,63 @@ TURN_RIGHT = robo.CODE_RSTurnRight
 NO_OP = robo.CODE_RSNoOp
 BURP = robo.CODE_RSBurp
 ROAR = robo.CODE_RSRoar
+RIGHTARM_UP = robo.CODE_RSRightArmUp
+RIGHTARM_DOWN = robo.CODE_RSRightArmDown
+LEFTARM_UP = robo.CODE_RSLeftArmUp
+LEFTARM_DOWN = robo.CODE_RSLeftArmDown
+
+
+# Controller Tweaks for Responsiveness Definitions
+DEAD_ZONE = 0.2
+LAST_LEFT_JOY_EVENT = 0
+LAST_RIGHT_JOY_EVENT = 0
+JOY_EVENT_DEDUP = 0.7
+
+def log_event(event):
+    try:
+        print("Event Type: {}, Event Button: {}, Event: {}".format(event.type,event.button,event))
+    except AttributeError:
+        print("Event Type: {}, Event Value: {}, Event: {}".format(event.type,event.value,event))
+
 
 
 while(IS_RUNNING):
     for event in pygame.event.get():
 
-        try:
-            print("Event Type: {}, Event Button: {}, Event: {}".format(event.type,event.button,event))
-        except AttributeError:
-            print("Event Type: {}, Event Value: {}, Event: {}".format(event.type,event.value,event))
-
         if event.type == JOYBUTTONUP and event.button == MENU_BUTTON:
             print("Quitting...")
             IS_RUNNING = False
+        if event.type == JOYAXISMOTION:# and event.axis == RIGHTSTICK_UPDOWN:
+            #Joy events arrive very quickly, we dont want to pay attention to all of them
+            if (event.value > DEAD_ZONE or event.value < (-1*DEAD_ZONE)):
+                #print("Last Joy Event: {} + Dedup Interval {} = {} < {} is {}".format(LAST_JOY_EVENT, JOY_EVENT_DEDUP, (LAST_JOY_EVENT + JOY_EVENT_DEDUP), currentTime,((LAST_JOY_EVENT + JOY_EVENT_DEDUP) < currentTime)))
+                currentTime = time.time()
+                if event.axis == RIGHTSTICK_UPDOWN:
+                    if (LAST_RIGHT_JOY_EVENT + JOY_EVENT_DEDUP) < currentTime:
+                        LAST_RIGHT_JOY_EVENT = currentTime
+                        if event.value > DEAD_ZONE: 
+                            log_event(event)
+                            print("Right - Down")
+                            myrobo.send_code(RIGHTARM_DOWN)
+                        elif event.value < (-1*DEAD_ZONE): 
+                            log_event(event)
+                            print("Right - Up")
+                            myrobo.send_code(RIGHTARM_UP)
+                        else:
+                            print("Zeroed")
+                elif event.axis == LEFTSTICK_UPDOWN:
+                    if (LAST_LEFT_JOY_EVENT + JOY_EVENT_DEDUP) < currentTime:
+                        LAST_LEFT_JOY_EVENT = currentTime
+                        if event.value > DEAD_ZONE: 
+                            log_event(event)
+                            print("Left - Down")
+                            myrobo.send_code(LEFTARM_DOWN)
+                        elif event.value < (-1*DEAD_ZONE): 
+                            log_event(event)
+                            print("Left - Up")
+                            myrobo.send_code(LEFTARM_UP)
+                        else:
+                            print("Zeroed")
         if event.type == JOYBUTTONUP and event.button == B_BUTTON:
             myrobo.send_code(BURP)
         if event.type == JOYBUTTONUP and event.button == A_BUTTON:
